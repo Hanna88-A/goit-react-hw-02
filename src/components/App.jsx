4ef46c76247bd93823352597a76dbfd1a9dc5c46
Hanna88-A <annamukolaivna88@gmail.com>
2022-02-15 00:33:34 +0200

    Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import {Component} from "react";
 import Section from "./Section/Section";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Statistics from "./Statistics/Statistics";
 import Notification from "./NotificationTitle/NotificationTitle";
 
-export class App extends React.Component {
+export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
